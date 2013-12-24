f41e88c09dbb2d5545eecd4d1b3a17c67d08e1fd
Matt Quinn <i@mattjquinn.com>
2013-12-24 08:17:50 -0500

    dmenu wasn't working after a recent package update; removing the (now apparently removed) '-m' flag solved the issue.

--- config.h
+++ config.h
@@ -51,7 +51,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "terminator", NULL };
 
 static Key keys[] = {
